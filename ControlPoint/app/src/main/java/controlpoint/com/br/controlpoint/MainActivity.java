3de36267d4985ff84264c78fd6c7c58ee7fb270b
Caitano Junior <caitanojunior@gmail.com>
2016-01-20 23:40:55 -0300

    start cod Control point Android

--- ControlPoint/app/src/main/java/controlpoint/com/br/controlpoint/MainActivity.java
+++ ControlPoint/app/src/main/java/controlpoint/com/br/controlpoint/MainActivity.java
@@ -1,9 +1,11 @@
 package controlpoint.com.br.controlpoint;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -34,5 +36,9 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void readClients(View v){
+        startActivityForResult(new Intent(this,ScheduleActivity.class), 1);
     }
 }
